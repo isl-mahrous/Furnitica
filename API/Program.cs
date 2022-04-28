6b14bedf89d3c3eec69e6d336053d08291de7eee
Islam Mahrous <isl.mahrous@gmail.com>
2022-04-29 01:53:58 +0200

    Context, connection string, some cleanup

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using Infrastructue.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Database Service
+builder.Services.AddDbContext<StoreContext>(options => options.
+            UseSqlServer(builder.Configuration.
+            GetConnectionString("Default")));
 
 var app = builder.Build();
 
