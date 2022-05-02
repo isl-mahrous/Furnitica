d5ccbff79f291803ffd29933306a45047f7ae2ea
hossam <hussam.said42@gmail.com>
2022-05-02 02:43:32 +0200

    setting up the productTypes Controller

---
+++ API/Controllers/ProductTypesController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductTypesController : ControllerBase
+    {
+        private readonly IGenericRepository<Product> _productRepo;
+        private readonly IGenericRepository<ProductType> _productTypeRepo;
+        private readonly IMapper _mapper;
+
+        public ProductTypesController(IGenericRepository<Product> productRepo,
+                                      IGenericRepository<ProductType> productTypeRepo,
+                                      IMapper mapper)
+        {
+            _productRepo = productRepo;
+            _productTypeRepo = productTypeRepo;
+            _mapper = mapper;
+        }       
+    }
+}
