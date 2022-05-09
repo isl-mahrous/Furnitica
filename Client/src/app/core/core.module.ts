7cfc9db17bab17bb256a4a3ed2348e975394a879
Islam Mahrous <66200462+isl-mahrous@users.noreply.github.com>
2022-05-09 15:57:50 +0200

    loading spinner, interceptor, breadcrumb, section header

--- Client/src/app/core/core.module.ts
+++ Client/src/app/core/core.module.ts
@@ -2,17 +2,20 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
+import { SectionHeaderComponent } from './section-header/section-header.component';
+import { BreadcrumbModule } from 'xng-breadcrumb';
 
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [NavBarComponent, SectionHeaderComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    BreadcrumbModule
   ],
   exports: [
-    NavBarComponent
+    NavBarComponent,
+    SectionHeaderComponent
   ]
 })
 export class CoreModule { }
