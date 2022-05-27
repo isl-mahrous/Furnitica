0632a0477b94d40211c6a7c880f5c8bfb4c9ff15
mohamed-fathy96 <pet.mohamedfathy@gmail.com>
2022-05-27 18:55:09 +0200

    added stripe key to environment production

--- Client/src/environments/environment.prod.ts
+++ Client/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://localhost:7272/api/"
+  apiUrl: "https://localhost:7272/api/",
+  stripeKey : "pk_test_51KyZpGLfkfZnEzSoBeteIizvBg64NexZoo3JpUoNqTuU9HIjw0GQCUo5ewqKG6OrqeHH4PZzZh4QL95lh1rU5cF500InZaAte0"
 };
